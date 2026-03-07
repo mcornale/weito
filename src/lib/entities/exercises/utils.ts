79de4b2fa03946cb9780c6943b7e81c33f287c07
mcornale <massimiliano.cornale@gmail.com>
2026-03-07 21:24:27 +0100

    fix sets and reps formatter

--- src/lib/entities/exercises/utils.ts
+++ src/lib/entities/exercises/utils.ts
@@ -2,7 +2,7 @@ import type { Exercise } from './types';
 
 export function formatSetsAndReps(setsAndReps: Exercise['setsAndReps']): string {
 	if (setsAndReps.every((set) => set.sets === 1))
-		return setsAndReps.map((set) => set.reps).join('-');
+		return setsAndReps.map((set) => set.reps).join(' · ');
 
 	return setsAndReps.map((set) => `${set.sets}×${set.reps}`).join(' · ');
 }
